73ab3973347412257d4dd1ec936ea699734ee259
Andrew Williams <1williamsaj@gmail.com>
2019-07-28 16:33:57 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -45,7 +45,26 @@ end
 #-------------------------------------------#
 
 
-def run
+def run(my_songs)
+  help()
+  input = prompt()
 
+  while input != "exit"
+    if input == "list"
+      list(my_songs)
+      input = prompt()
+    elsif input == "play"
+      play(my_songs)
+      input = prompt()
+    elsif input == "help"
+      help()
+      input = prompt()
+    else
+      puts "Invalid command"
+      help()
+      input = prompt()
+    end
+  end
 
+  exit_jukebox()
 end
